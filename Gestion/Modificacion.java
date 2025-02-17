a020d9d0e68f00863ac41a45f7b9a0f7e3def14b
Matheus <matheusflorenciotj@gmail.com>
2025-02-17 20:40:53 +0100

    Implementado el metodo de Modificacion

--- Gestion/Modificacion.java
+++ Gestion/Modificacion.java
@@ -4,7 +4,7 @@ public class Modificacion {
     public void actualizarNota(int id, String nombreAsignatura, double nuevaNota) {
         for (Estudiante estudiante : Listado.estudiantes) {
             if (estudiante.getId() == id) {
-                for (Asignatura asignatura : estudiante.getAsignaturas()) {
+                for (Asignatura asignatura : estudiante.getAsignaturas()){
                     if (asignatura.getNombre().equals(nombreAsignatura)) {
                         asignatura.setNota(nuevaNota);
                     }
