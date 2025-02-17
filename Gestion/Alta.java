65849ae1293774d2ccafbdf31cc78687c8489407
Matheus <matheusflorenciotj@gmail.com>
2025-02-17 20:36:16 +0100

    Implementacion del metodo Alta

--- Gestion/Alta.java
+++ Gestion/Alta.java
@@ -6,15 +6,15 @@ public class Alta {
     public void crearEstudiantes() {
         for (int i = 0; i < 2; i++) {
             System.out.println("Introduce los datos del estudiante " + (i + 1));
-            int id = Input.obtenerInt("Introduce el ID del estudiante:");
-            String nombre = Input.obtenerString("Introduce el nombre completo del estudiante:");
-            int edad = Input.obtenerInt("Introduce la edad del estudiante:");
-            String fechaNacimiento = Input.obtenerString("Introduce la fecha de nacimiento del estudiante (DD/MM/AAAA):");
+            int id = Input.obtenerInt("Introduce el ID del estudiante: ");
+            String nombre = Input.obtenerString("Introduce el nombre completo del estudiante: ");
+            int edad = Input.obtenerInt("Introduce la edad del estudiante: ");
+            String fechaNacimiento = Input.obtenerString("Introduce la fecha de nacimiento del estudiante (DD/MM/AAAA): ");
 
-            String asignatura1Nombre = Input.obtenerString("Introduce el nombre de la primera asignatura:");
-            double asignatura1Nota = Input.obtenerDouble("Introduce la nota de la primera asignatura:");
-            String asignatura2Nombre = Input.obtenerString("Introduce el nombre de la segunda asignatura:");
-            double asignatura2Nota = Input.obtenerDouble("Introduce la nota de la segunda asignatura:");
+            String asignatura1Nombre = Input.obtenerString("Introduce el nombre de la primera asignatura: ");
+            double asignatura1Nota = Input.obtenerDouble("Introduce la nota de la primera asignatura: ");
+            String asignatura2Nombre = Input.obtenerString("Introduce el nombre de la segunda asignatura: ");
+            double asignatura2Nota = Input.obtenerDouble("Introduce la nota de la segunda asignatura: ");
 
             Asignatura asignatura1 = new Asignatura(asignatura1Nombre, asignatura1Nota);
             Asignatura asignatura2 = new Asignatura(asignatura2Nombre, asignatura2Nota);
